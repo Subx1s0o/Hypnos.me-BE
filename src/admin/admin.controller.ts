70df1245580a787969e0c86005ef4782899c5b23
Subx1s0o <nazarsabardin@gmail.com>
2024-10-25 00:37:49 +0300

    feat: add changing roles

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Query, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AdminGuard } from './admin.guard';
 import { AdminService } from './admin.service';
@@ -9,8 +9,16 @@ import { AdminService } from './admin.service';
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Get('hello')
-  async getHello() {
-    return 'Okay hello';
+  @Get()
+  async getAdmins() {
+    return await this.adminService.getAdmins();
+  }
+
+  @Patch(':id')
+  async changeUserRole(
+    @Query('id') id: string,
+    @Body() role: 'admin' | 'user',
+  ) {
+    return await this.adminService.changeUserRole(id, role);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@lib/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAdmins() {
+    return await this.prisma.users.findMany({ where: { role: 'admin' } });
+  }
+
+  async changeUserRole(id: string, role: 'admin' | 'user') {
+    const user = await this.prisma.users.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`The user with id ${id} wasn't found`);
+    }
+
+    await this.prisma.users.update({ where: { id }, data: { role: role } });
+
+    throw new HttpException(
+      'The user role was successfully updated',
+      HttpStatus.OK,
+    );
+  }
+}
