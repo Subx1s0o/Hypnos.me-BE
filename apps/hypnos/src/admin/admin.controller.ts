dffce31140fb4e11866c010d5bea8c35b7d1cec3
Subx1s0o <nazarsabardin@gmail.com>
2024-11-12 23:04:45 +0200

    fix: add auth to  admin controller

--- apps/hypnos/src/admin/admin.controller.ts
+++ apps/hypnos/src/admin/admin.controller.ts
@@ -1,3 +1,4 @@
+import { Auth } from '@lib/entities/decorators';
 import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { User } from 'types';
@@ -5,24 +6,25 @@ import { CreateAdminDto } from './dtos/create.dto';
 
 import { AdminService } from './admin.service';
 
-import { Auth } from '@lib/entities/decorators/Auth';
-
 @Controller('admin')
 @ApiTags('admin')
 @Auth('owner')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
+  @Auth('owner')
   @Get()
   async getAdmins(): Promise<Omit<User, 'password'>[]> {
     return await this.adminService.getAdmins();
   }
 
+  @Auth('owner')
   @Post()
   async changeRole(@Body() data: CreateAdminDto) {
     return await this.adminService.changeRole(data);
   }
 
+  @Auth('owner')
   @Delete(':id')
   async deleteAdmin(@Query('id') id: string) {
     return await this.adminService.deleteAdmin(id);
