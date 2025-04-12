21571fc3b11e1550b72a4c631938a4558c9235fa
Subx1s0o <nazarsabardin@gmail.com>
2025-04-12 18:12:41 +0300

    fix: fixing user service

--- src/app/http/user/user.controller.ts
+++ src/app/http/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Auth } from '@/libs/entities';
-import { Body, Controller, Get, Post, Req } from '@nestjs/common/decorators';
+import { Controller, Get, Req } from '@nestjs/common/decorators';
 
 import { Request } from 'express';
 import { UserService } from './user.service';
@@ -10,19 +10,7 @@ export class UserController {
 
   @Auth()
   @Get()
-  async getUser(@Req() req: Request & { user: string }) {
-    return await this.userService.getUser(req.user);
-  }
-
-  @Get('favorites')
-  @Auth()
-  async getFavorits(@Req() req: Request & { user: string }) {
-    return await this.userService.getFavorites(req.user);
-  }
-
-  @Auth()
-  @Post('favorites')
-  async addTofavorites(@Body() data, @Req() req: Request & { user: string }) {
-    return await this.userService.createFavorite(req.user, data.productId);
+  async getUser(@Req() req: Request & { user: { id: string } }) {
+    return await this.userService.getUser(req.user.id);
   }
 }
--- src/app/http/user/user.service.ts
+++ src/app/http/user/user.service.ts
@@ -1,4 +1,5 @@
 import { PrismaService } from '@/libs/common';
+import { NotFoundException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common/decorators';
 
 @Injectable()
@@ -9,88 +10,9 @@ export class UserService {
     const user = await this.prisma.users.findUnique({
       where: { id: userId },
     });
-    return user;
-  }
-
-  async getFavorites(userId: string) {
-    const favorites = await this.prisma.favorites.findUnique({
-      where: { userId },
-      include: {
-        items: {
-          include: {
-            product: true,
-          },
-        },
-      },
-    });
-
-    if (!favorites || !favorites.items.length) {
-      return [];
-    }
-
-    return favorites.items.map((item) => ({
-      productId: item.productId,
-      productTitle: item.product.title,
-      productSlug: item.product.slug,
-      productPrice: item.product.price,
-      productPhoto: item.product.media.main.url,
-    }));
-  }
-
-  async createFavorite(userId: string, productId: string) {
-    //Перевірити айді на валідність
-    const existingFavoriteItem = await this.prisma.favoritesItem.findFirst({
-      where: {
-        favorites: {
-          userId,
-        },
-        productId,
-      },
-    });
-
-    if (existingFavoriteItem) {
-      throw new Error('Product is already in favorites');
-    }
-
-    const favorites = await this.prisma.favorites.upsert({
-      where: { userId },
-      create: {
-        userId,
-        items: {
-          create: [{ productId }],
-        },
-      },
-      update: {
-        items: {
-          create: [{ productId }],
-        },
-      },
-      include: {
-        items: true,
-      },
-    });
-
-    return favorites;
-  }
-
-  async deleteFavorite(userId: string, productId: string) {
-    const favoriteItem = await this.prisma.favoritesItem.findFirst({
-      where: {
-        favorites: {
-          userId,
-        },
-        productId,
-      },
-    });
-
-    if (!favoriteItem) {
-      throw new Error('Product is not in favorites');
-    }
-
-    await this.prisma.favoritesItem.delete({
-      where: { id: favoriteItem.id },
-    });
-
-    return { message: 'Favorite successfully deleted' };
+    if (!user) throw new NotFoundException('User not found');
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
   }
 }
